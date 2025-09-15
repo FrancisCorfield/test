9c464057fe2393f5b08b5ee643481fa7ca075d4c
FrancisCorfield <zefennekinfan@outlook.com>
2025-09-15 09:31:47 -0400

    commit du cegep apres clonage

--- premier_test_maison.py
+++ premier_test_maison.py
@@ -6,3 +6,13 @@ print(grrr)
 #test de "futur modification"
 
 print("J'ai ENFIN REUSSI AAAAAAAAAAAAAAAAAAAAAAA")
+
+# j'ai enfin compris comment ca marche
+
+#cd ..
+#git clone git@github.com:FrancisCorfield/test.git
+
+# pi la boom le dossier test a été pull sur mon desktop de cegep
+
+# maintenant, on va essayer de send ces lignes de commentaires sur github
+
